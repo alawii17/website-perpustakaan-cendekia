15c23b19dbcb8f8e51622f775e12a3106a540ada
alawii17 <alawialatas1414@gmail.com>
2024-11-11 03:30:07 +0700

    Membuat trait untuk fungsi upload, update, dan delete file untuk model

---
+++ app/Traits/HasFile.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+trait Hasfile
+{
+    public function upload_file(Request $request, string $colomn, string $folder): ?string
+    {
+        return $request->Hasfile($colomn) ? $request->file($colomn)->store($folder) : null;
+    }
+
+    public function update_file(Request $request, Model $model, string $colomn, string $folder): ?string
+    {
+        if ($request->hasFile($colomn)) {
+            if ($model->$colomn) {
+                Storage::delete($model->$colomn);
+            }
+            $thumbnail = $request->file($colomn)->store($folder);
+        } else {
+            $thumbnail = $model->$colomn;
+        }
+
+        return $thumbnail;
+    }
+
+    public function delete_file(Model $model, string $colomn): void
+    {
+        if ($model->column) {
+            Storage::delete($model->$colomn);
+        }
+    }
+}
