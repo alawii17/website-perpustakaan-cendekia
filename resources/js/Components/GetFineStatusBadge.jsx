9efe341239d3e5596ff6618a29b2e5fc3397b550
alawii17 <alawialatas1414@gmail.com>
2024-11-13 01:13:24 +0700

    Penambahan Utilitas dan penerapan prettier

--- resources/js/Components/GetFineStatusBadge.jsx
+++ resources/js/Components/GetFineStatusBadge.jsx
@@ -1,11 +1,11 @@
 import { FINEPAYMENTSTATUS } from '@/lib/utils';
 import { Badge } from './Components/ui/badge';
 
-export default function GetFineStatusBadge({ status }){
+export default function GetFineStatusBadge({ status }) {
     const { PENDING, SUCCESS, FAILED } = FINEPAYMENTSTATUS;
     let badge, text;
 
-    switch(status){
+    switch (status) {
         case PENDING:
             badge = 'text-white bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-500 bg-yellow-500';
             text = PENDING;
@@ -22,5 +22,5 @@ export default function GetFineStatusBadge({ status }){
             badge = '';
             text = '-';
     }
-    return <Badge className={badge}>{text}</Badge>
+    return <Badge className={badge}>{text}</Badge>;
 }
