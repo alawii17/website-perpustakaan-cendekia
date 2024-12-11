cad4cd07e4aed16bc9d74d9b9c67db49ae006c5e
alawii17 <alawialatas1414@gmail.com>
2024-12-12 01:09:23 +0700

    Implementasi Permission spatie

--- resources/js/Layouts/Partials/SidebarResponsive.jsx
+++ resources/js/Layouts/Partials/SidebarResponsive.jsx
@@ -80,7 +80,12 @@ export default function SidebarResponsive({ url, auth }) {
                     title="Peran"
                     icon={IconCircleKey}
                 />
-                <NavLinkResponsive url="#" title="Izin" icon={IconVersions} />
+                <NavLink
+                    url={route('admin.permissions.index')}
+                    active={url.startsWith('/admin/permissions')}
+                    title="Izin"
+                    icon={IconVersions}
+                />
                 <NavLinkResponsive url="#" title="Tetapkan Izin" icon={IconKeyframe} />
                 <NavLinkResponsive url="#" title="Tetapkan Peran" icon={IconLayoutKanban} />
                 <NavLinkResponsive url="#" title="Akses Rute" icon={IconRoute} />
